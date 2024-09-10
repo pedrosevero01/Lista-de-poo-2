99cf73f008d58b24a3239ed1460b73b3baea83e3
João Pedro Severo <joaopedroseveros2@gmail.com>
2024-09-09 23:45:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-var Lado = new Array(3);
+function Media(Nota1, Nota2) {
+    return (Nota1 + Nota2) / 2;
+}
 
-for (var lado = 0; lado < 3; lado++) {
-    Lado[lado] = Number(prompt("Insira o Lado " + (lado + 1)));
+var maior, Planilha = new Array(5);
 
-    while (Lado[lado] <= 0) {
-        Lado[lado] = Number(prompt("Valor invalido, insira o lado " + (lado + 1) + " novamente"));
+for (var Qntd_Alunos = 0; Qntd_Alunos < 5; Qntd_Alunos++) {
+    Planilha[Qntd_Alunos] = new Array(4);
+    for (var Qntd_Notas = 0; Qntd_Notas < 2; Qntd_Notas++) {
+        Planilha[Qntd_Alunos][Qntd_Notas] = Number(prompt("Informe a nota " + (Qntd_Notas + 1) + " do aluno " + (Qntd_Alunos + 1) + ": "));
     }
 }
 
-if (Lado[0] < Lado[1] + Lado[2] && Lado[1] < Lado[0] + Lado[2] && Lado[2] < Lado[0] + Lado[1]) {
-    console.log("Os Valores formam um triangulo!");
-
-    if (Lado[0] == Lado[1] && Lado[0] == Lado[2]) {
-        console.log("O triangulo e Equilatero!");
-
-    } else if (Lado[0] == Lado[1] && Lado[0] != Lado[2] || Lado[0] == Lado[2] && Lado[0] != Lado[1] || Lado[1] == Lado[2] && Lado[1] != Lado[0]) {
-        console.log("O triangulo e Isoceles!");
-
-    } else if (Lado[0] != Lado[1] && Lado[0] != Lado[2] && Lado[1] != Lado[2]) {
-        console.log("O triangulo e Escaleno!");
-
+for (var Aluno = 0; Aluno < 5; Aluno++) {
+    Planilha[Aluno][3] = Media(Planilha[Aluno][0], Planilha[Aluno][1]);
+    if (Planilha[Aluno][3] >= 5) {
+        console.log("Aluno " + (Aluno + 1) + " aprovado!");
+        Planilha[Aluno][2] = 0;
+    } else {
+        console.log("Aluno " + (Aluno + 1) + " reprovado e precisa fazer a prova 3");
+        Planilha[Aluno][2] = Number(prompt("Insira a nota da Av3 do Aluno " + (Aluno + 1) + ": "));
+        if (Planilha[Aluno][0] >= Planilha[Aluno][1]) {
+            maior = Planilha[Aluno][0];
+        } else {
+            maior = Planilha[Aluno][1];
+        }
+        Planilha[Aluno][3] = Media(Planilha[Aluno][2], maior);
     }
+}
 
-} else {
-    console.log("Os Valores nao formam um triangulo!");
+console.log("\nNotas dos alunos:");
+for (var Aluno = 0; Aluno < 5; Aluno++) {
+    console.log("Aluno " + (Aluno + 1) + ": ");
+    for (var Nota = 0; Nota < 4; Nota++) {
+        console.log(Planilha[Aluno][Nota].toFixed(1) + " ");
+    }
+    if (Planilha[Aluno][3] >= 5) {
+        console.log(" Aluno Aprovado! ");
+    } else {
+        console.log(" Aluno reprovado! ");
+    }
 }
